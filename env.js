6761fe5930fad6b6078116049202b76af16eeafd
david.gogovie <david.gogovie@yahoo.com>
2023-03-18 21:09:58 +0000

    first commit

--- env.js
+++ env.js
@@ -8,7 +8,7 @@
 // import { Platform } from 'react-native'
 
 let port_local_client = 3000
-let port_local_server = 4000
+let port_local_server = 4001
 let port_remote_client = 3000
 let port_remote_server = 4000
 // let host_local_client = '192.168.56.1'
@@ -17,8 +17,8 @@ let port_remote_server = 4000
 // let host_local_server = '192.168.100.6'
 let host_local_client = '127.0.0.1'
 let host_local_server = '127.0.0.1'
-let host_remote_client = 'https://eodh-b.onrender.com'
-let host_remote_server = 'https://eodh-b.onrender.com'
+let host_remote_client = 'https://eodh-b2.onrender.com'
+let host_remote_server = 'https://eodh-b2.onrender.com'
 // let url_local_client = 'http://192.168.56.1:3000'
 // let url_local_server = 'http://192.168.56.1:4000'
 // let url_local_client = 'http://192.168.100.6:3000'
@@ -27,21 +27,21 @@ let host_remote_server = 'https://eodh-b.onrender.com'
 // let url_local_server = 'http://0.0.0.0:4000'
 let url_local_client = 'http://127.0.0.1:3000'
 let url_local_server = 'http://127.0.0.1:4000'
-let url_remote_client = 'https://eodh-b.onrender.com'
-let url_remote_server = 'https://eodh-b.onrender.com'
+let url_remote_client = 'https://eodh-b2.onrender.com'
+let url_remote_server = 'https://eodh-b2.onrender.com'
 // if (Platform.OS === 'android') {
 // if (global.osPlatform === 'android') {
     // url_local_client = 'http://10.0.2.2:3000'
     // url_local_server = 'http://10.0.2.2:4000'
-    url_local_client = 'https://eodh-b.onrender.com'
-    url_local_server = 'https://eodh-b.onrender.com'
+    url_local_client = 'https://eodh-b2.onrender.com'
+    url_local_server = 'https://eodh-b2.onrender.com'
 // }
-let database_port = 8000
+let database_port = 8001
 // let database_url = 'http://192.168.56.1:8000'
 // let database_url = 'http://192.168.100.6:8000'
 // let database_url = 'http://127.0.0.1:8000'
 // let database_url = 'http://0.0.0.0:8000'
-let database_url = 'https://eodh-b.onrender.com'
+let database_url = 'https://eodh-b2.onrender.com'
 
 let env = {
     port_local_client: port_local_client,
